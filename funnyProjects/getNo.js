aa00cc640c95af06df774f4e9edb8a2ed83be5f6
pomodoroCeleste <168738794+pomodoroCeleste@users.noreply.github.com>
2024-05-03 10:58:25 +0200

    Delete funnyProjects directory

--- funnyProjects/getNo.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('no').addEventListener('mouseover', function() {
-  var xPos = Math.floor(Math.random() * window.innerWidth);
-  var yPos = Math.floor(Math.random() * window.innerHeight);
-  document.getElementById('no').style.position = 'absolute';
-  document.getElementById('no').style.left = xPos + 'px';
-  document.getElementById('no').style.top = yPos + 'px';
-});
